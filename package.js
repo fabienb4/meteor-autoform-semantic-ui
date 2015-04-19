5d38921ed3bae75ae5bce48a6c8070260087d19b
FabienB4 <fabienb4.b4soft@gmail.com>
2015-04-18 20:04:42 -0600

    0.3.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'fabienb4:autoform-semantic-ui',
-  version: '0.2.0',
+  version: '0.3.0',
   // Brief, one-line summary of the package.
   summary: 'Semantic-ui template for aldeed:autoform package.',
   // URL to the Git repository containing the source code for this package.
@@ -53,8 +53,6 @@ Package.onUse(function(api) {
     'templates/semantic-ui/inputTypes/select-checkbox/select-checkbox.js',
     'templates/semantic-ui/inputTypes/select-radio/select-radio.html',
     'templates/semantic-ui/inputTypes/select-radio/select-radio.js',
-    'templates/semantic-ui/inputTypes/select-search/select-search.html',
-    'templates/semantic-ui/inputTypes/select-search/select-search.js',
     'templates/semantic-ui/inputTypes/submit/submit.html',
     'templates/semantic-ui/inputTypes/tel/tel.html',
     'templates/semantic-ui/inputTypes/text/text.html',
