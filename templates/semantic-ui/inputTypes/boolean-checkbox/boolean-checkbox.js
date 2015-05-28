c94df3980bcb42ed5b56a514ac0bc830e824b041
Richard <henk.exe@gmail.com>
2015-05-28 14:20:22 +0200

    Add option to change Boolean (checkbox) type

--- templates/semantic-ui/inputTypes/boolean-checkbox/boolean-checkbox.js
+++ templates/semantic-ui/inputTypes/boolean-checkbox/boolean-checkbox.js
@@ -1,3 +1,23 @@
 Template.afCheckbox_semanticUI.onRendered(function() {
   $(this.firstNode).checkbox();
 });
+
+Template.afCheckbox_semanticUI.helpers({
+  /*
+    Adds option to change checkbox type
+    Options are:
+      checkbox (default)
+      slider   (http://semantic-ui.com/modules/checkbox.html#slider)
+      toggle   (http://semantic-ui.com/modules/checkbox.html#toggle)
+  */
+  checkboxType: function () {
+    switch (this.atts["checkbox-type"]) {
+      case "slider": return "slider ";
+      case "toggle": return "toggle ";
+      default: return;
+    }
+  },
+  itemAtts: function () {
+    return _.omit(this.atts, "checkbox-type")
+  }
+});
