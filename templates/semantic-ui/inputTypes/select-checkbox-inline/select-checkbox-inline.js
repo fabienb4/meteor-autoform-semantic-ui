7d939d994d24103bc5aa2f4a6c1c021eac2c8d06
FabienB4 <fabienb4.b4soft@gmail.com>
2015-07-27 11:29:25 -0600

    Added `select-checkbox-inline` & `select-radio-inline` input types.

---
+++ templates/semantic-ui/inputTypes/select-checkbox-inline/select-checkbox-inline.js
@@ -0,0 +1,24 @@
+Template.afCheckboxGroupInline_semanticUI.helpers({
+  atts: function selectedAttsAdjust() {
+    var atts = _.clone(this.atts);
+
+    if (this.selected) {
+      atts.checked = "checked";
+    }
+
+    // remove data-schema-key attribute because we put it
+    // on the entire group
+    delete atts["data-schema-key"];
+
+    return atts;
+  },
+  dsk: function dsk() {
+    return {
+      "data-schema-key": this.atts["data-schema-key"]
+    };
+  }
+});
+
+Template.afCheckboxGroupInline_semanticUI.onRendered(function() {
+  $(this.firstNode).find(".ui.checkbox").checkbox();
+});
