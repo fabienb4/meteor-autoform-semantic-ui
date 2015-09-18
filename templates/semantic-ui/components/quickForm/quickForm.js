ea7e4ddccea83b32c5bff1e1c2c6f36368c74d01
FabienB4 <fabienb4.b4soft@gmail.com>
2015-09-18 15:41:47 -0600

    Added support for grouping fields (See aldeed/autoform README).
    Semantic-UI implementation uses `ui segment` instead of `fieldset` for better rendering.

--- templates/semantic-ui/components/quickForm/quickForm.js
+++ templates/semantic-ui/components/quickForm/quickForm.js
@@ -1,4 +1,20 @@
 Template.quickForm_semanticUI.helpers({
+  fieldGroupLabel: function () {
+    var name = this.name;
+
+    // if field group name is of the form XY_abcde where "XY" is a number, remove prefix
+    if (! isNaN(parseInt(name.substr(0,2), 10)) && name.charAt(2) === "_") {
+      name = name.substr(3);
+    }
+
+    // if SimpleSchema.defaultLabel is defined, use it
+    if (typeof SimpleSchema.defaultLabel === "function") {
+      return SimpleSchema.defaultLabel(name);
+    } else {
+      // else, just capitalise name
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+  },
   quickFieldsAtts: function () {
     // These are the quickForm attributes that we want to forward to
     // the afQuickFields component.
